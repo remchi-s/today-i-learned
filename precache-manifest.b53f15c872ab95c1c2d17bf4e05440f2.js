40b7dcefa262c435a652012526ed1db19167d449
remchi-s <remchi-s@users.noreply.github.com>
2020-05-30 22:30:16 +0000

    deploy: 306f1e3d2b0614d1faec485a66a5a0e1d07de9b1

---
+++ precache-manifest.b53f15c872ab95c1c2d17bf4e05440f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df619f13dd8533adf3df4e02279fac69",
+    "url": "https://remchi-s.github.io/today-i-learned/index.html"
+  },
+  {
+    "revision": "8b2c6a60624ef8e4c6f6",
+    "url": "https://remchi-s.github.io/today-i-learned/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97f2b96cb0cde0352550",
+    "url": "https://remchi-s.github.io/today-i-learned/static/js/2.743edd3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://remchi-s.github.io/today-i-learned/static/js/2.743edd3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2c6a60624ef8e4c6f6",
+    "url": "https://remchi-s.github.io/today-i-learned/static/js/main.66d69da7.chunk.js"
+  },
+  {
+    "revision": "78cf468cb9ce1945e85e",
+    "url": "https://remchi-s.github.io/today-i-learned/static/js/runtime-main.921c5312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://remchi-s.github.io/today-i-learned/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://remchi-s.github.io/today-i-learned/precache-manifest.b53f15c872ab95c1c2d17bf4e05440f2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://remchi-s.github.io/today-i-learned/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
